b68d889d6a565ce4bf55e7d88905f262a7e71f99
oliiveiraisa <isaaholiveira12@gmail.com>
2024-03-05 22:01:14 -0300

    Conteúdo do dia 05-02-2024

--- NP24_Java2_B/src/br/com/senaisp/bauru/isadora/secao01/HelloWorld.java
+++ NP24_Java2_B/src/br/com/senaisp/bauru/isadora/secao01/HelloWorld.java
@@ -1,3 +1,5 @@
+package br.com.senaisp.bauru.isadora.secao01;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/isadora/secao02/Text01.java
@@ -0,0 +1,22 @@
+package br.com.senaisp.bauru.isadora.secao02;
+
+public class Text01 {
+	/**
+	 * Comentário de documentação
+	 * @param arg
+	 */
+    public static void main(String[] args) {
+    	//Comentário de uma linha
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        /*
+         * Comentário de mais de uma linha 
+         **/
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }    
+}
